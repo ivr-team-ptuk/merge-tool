f2e5acbde47c072e17a3e14d9dfaf665197db94b
TameemKh <tameemkhanfar@gmail.com>
2026-05-22 23:28:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ function toggleNavbar() {{
 # RENDER HTML
 # =========================
 
-components.html( html_content, height=950, scrolling=True )
+components.html( html_content, height=200, scrolling=True )
 
 # =========================
 # HEADER
