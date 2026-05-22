23cae797a864e0457964fd5c2650e800bb586558
TameemKh <tameemkhanfar@gmail.com>
2026-05-22 23:32:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ function toggleNavbar() {{
 # RENDER HTML
 # =========================
 
-components.html( html_content )
+components.html( html_content, gap=0 )
 
 # =========================
 # HEADER
